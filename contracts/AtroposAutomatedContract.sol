437254b6cb4a345945838f95aebea47d094a50b1
Poor Miner <boyuanxu@usc.edu>
2021-06-30 00:22:38 -0700

    Updated to HackMoney specs

--- contracts/AtroposAutomatedContract.sol
+++ contracts/AtroposAutomatedContract.sol
@@ -1,11 +1,13 @@
 //SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.6.0;
+pragma solidity ^0.6.0; // We are only using 0.6.0 for Chainlink compatibility
 pragma experimental ABIEncoderV2; // This is acceptable as it is used by default in Solidity 0.8.0.
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/math/SafeMath.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@chainlink/contracts/src/v0.6/ChainlinkClient.sol";
 import "./external/KeeperCompatibleInterface.sol";
+import "./external/dai.sol";
 import "./IAtroposAutomatedContractExecutor.sol";
 import "./IAtroposAutomatedContract.sol";
 
@@ -17,68 +19,86 @@ contract AtroposAutomatedContract is
 {
     using SafeMath for uint256;
 
-    IAtroposAutomatedContractExecutor private _atroposExecutor;
+    // Kovan DAI
+    Dai public constant _daiInstance =
+        Dai(0x4F96Fe3b7A6Cf9725f59d353F723c1bDb64CA6Aa);
+    // Kovan LINK
+    IERC20 public constant _linkInstance =
+        IERC20(0xa36085F69e2889c224210F603D836748e7dC0088);
+
+    address public _beneficiary;
+
     uint256 public _jobStartBlock;
-    uint256 public _jobEndBlock;
+    uint256 public _expiration;
     uint256 public _jobInterval;
-    uint256 public _jobExpectedTriggers;
     bytes32 private _jobId;
     address private _oracle;
     uint256 private _fee;
 
-    string[] public _urlMapping;
-    uint256 public _urlMappingIndex;
-    string[] public _pathMapping;
-    bytes32[] public _expectedResultMapping;
-
+    string public _url;
+    string[] public _milestones;
+    uint256 public _milestonesIndex;
+    uint256[] public _rewards;
+    string public _path;
+    bytes32 public _expectedResult;
     bool public _responsePending;
     uint256 public _jobLastRun;
 
     constructor(
-        uint256 jobStartBlock,
-        uint256 jobEndBlock,
-        uint256 jobInterval,
-        uint256 jobExpectedRuns,
-        bytes32 jobId,
-        address oracle,
-        uint256 fee,
-        string[] memory urls,
-        string[] memory paths,
-        bytes32[] memory expectedResults
+        uint256 expiration, // seconds since epoch
+        address beneficiary,
+        string memory url,
+        string[] memory milestones,
+        uint256[] memory rewards,
+        uint256 totalRewards,
+        uint256 permitDeadline,
+        uint8 v,
+        bytes32 r,
+        bytes32 s
     ) public {
         setPublicChainlinkToken();
-        _jobStartBlock = jobStartBlock;
+        _beneficiary = beneficiary;
+        _jobStartBlock = block.number;
         _jobLastRun = _jobStartBlock.sub(1);
-        _jobEndBlock = jobEndBlock;
-        _jobInterval = jobInterval;
-        _jobExpectedTriggers = jobExpectedRuns;
-        _jobId = jobId;
-        _oracle = oracle;
+        _expiration = expiration;
+        _url = url;
+        _milestones = milestones;
+        _milestonesIndex = 0;
+        _rewards = rewards;
+        _path = "open_issues";
+        _expectedResult = 0x0;
+        _jobInterval = 6 * 60 * 60; // 6 hours
+        _jobId = "50fc4215f89443d185b061e5d7af9490";
+        _oracle = 0x2f90A6D021db21e1B2A077c5a37B3C7E75D15b7e;
         _fee = 0.1 * 10**18;
-
-        require(
-            (urls.length == paths.length) &&
-                (paths.length == expectedResults.length),
-            "Bad url info"
-        );
-        _urlMappingIndex = 0;
-        for (uint256 i = 0; i < urls.length; ++i) {
-            _urlMapping[i] = urls[i];
-        }
-        for (uint256 i = 0; i < paths.length; ++i) {
-            _pathMapping[i] = paths[i];
-        }
-        for (uint256 i = 0; i < expectedResults.length; ++i) {
-            _expectedResultMapping[i] = expectedResults[i];
-        }
-
         _responsePending = false;
+
+        uint256 calculatedTotalRewards = 0;
+        for (uint256 i = 0; i < rewards.length; ++i) {
+            calculatedTotalRewards += rewards[i];
+        }
+        require(calculatedTotalRewards == totalRewards, "Reward mismatch");
+        _daiInstance.permit(
+            _msgSender(),
+            address(this),
+            _daiInstance.nonces(_msgSender()),
+            permitDeadline,
+            true,
+            v,
+            r,
+            s
+        );
+        _daiInstance.transferFrom(_msgSender(), address(this), totalRewards);
     }
 
-    // function setExecutor(IAtroposAutomatedContractExecutor atroposExecutor) external onlyOwner {
-    //     require(address(_atroposExecutor) == address(0), "Executor set");
-    //     _atroposExecutor = atroposExecutor;
-    // }
+    function withdraw() external onlyOwner {
+        require(_expiration < block.timestamp, "Not expired");
+        uint256 remainingDai = _daiInstance.balanceOf(address(this));
+        _daiInstance.transfer(owner(), remainingDai);
+        uint256 remainingLink = _linkInstance.balanceOf(address(this));
+        _linkInstance.transfer(owner(), remainingLink);
+        emit AtroposFundsReclaimed(remainingDai, remainingLink);
+    }
 
     function sendGetRequest() internal returns (bytes32 requestId) {
         require(!_responsePending, "REQ_PENDING");
@@ -87,8 +107,8 @@ contract AtroposAutomatedContract is
             address(this),
             this.getCallback.selector
         );
-        request.add("get", _urlMapping[_urlMappingIndex]);
-        request.add("path", _pathMapping[_urlMappingIndex]);
+        request.add("get", _url);
+        request.add("path", _milestones[_milestonesIndex]);
         _responsePending = true;
         return sendChainlinkRequestTo(_oracle, request, _fee);
     }
@@ -98,14 +118,17 @@ contract AtroposAutomatedContract is
         recordChainlinkFulfillment(requestId)
     {
         _responsePending = false;
-        emit AtroposResultReceived(
-            _expectedResultMapping[_urlMappingIndex],
-            result
+        emit AtroposResultReceived(_expectedResult, result);
+        if (result != _expectedResult) return;
+        uint256 rewards = _rewards[_milestonesIndex];
+        _daiInstance.transfer(_beneficiary, rewards);
+        ++_milestonesIndex;
+        emit AtroposExecutionTriggered(
+            _url,
+            _milestones[_milestonesIndex],
+            _beneficiary,
+            rewards
         );
-        if (result != _expectedResultMapping[_urlMappingIndex]) return;
-        ++_urlMappingIndex;
-        emit AtroposExecutionTriggered(address(_atroposExecutor));
-        // _atroposExecutor.execute();
     }
 
     function checkUpkeep(bytes calldata checkData)
@@ -114,15 +137,24 @@ contract AtroposAutomatedContract is
         override
         returns (bool upkeepNeeded, bytes memory performData)
     {
-        // require(address(_atroposExecutor) != address(0), "Executor not set");
+        return _checkUpkeep(checkData);
+    }
+
+    function _checkUpkeep(bytes memory checkData)
+        internal
+        view
+        returns (bool upkeepNeeded, bytes memory performData)
+    {
         bool jobCanRun = (block.number > _jobStartBlock) &&
-            (block.number < _jobEndBlock);
+            (block.number < _expiration);
         bool jobShouldRun = (block.number.sub(_jobLastRun)) >= _jobInterval;
         upkeepNeeded = jobCanRun && jobShouldRun;
         performData = checkData;
     }
 
     function performUpkeep(bytes calldata performData) external override {
+        (bool upkeepNeeded, bytes memory p) = _checkUpkeep("0");
+        require(upkeepNeeded, "Should not upkeep");
         emit AtroposUpkeepPerformed();
         _jobLastRun = block.number;
         sendGetRequest();
--- contracts/IAtroposAutomatedContract.sol
+++ contracts/IAtroposAutomatedContract.sol
@@ -4,5 +4,11 @@ pragma solidity ^0.6.0;
 interface IAtroposAutomatedContract {
     event AtroposUpkeepPerformed();
     event AtroposResultReceived(bytes32 expected, bytes32 actual);
-    event AtroposExecutionTriggered(address executor);
+    event AtroposExecutionTriggered(
+        string url,
+        string milestone,
+        address beneficiary,
+        uint256 dai
+    );
+    event AtroposFundsReclaimed(uint256 dai, uint256 link);
 }
